86969d9a3e4f2c660f965ea50c47cdc69f45bf0d
Rafael Kotovicz <rafaelrck21@gmail.com>
2024-03-02 23:17:08 -0300

    atualizando arquivos do dia 09

--- dia09/ex/ex1.js
+++ dia09/ex/ex1.js
@@ -5,7 +5,8 @@ let altura = prompt('Digite sua altura');
 let prof = prompt('Digite sua profissão');
 let imc = peso / (altura * altura);
 let condicao;
-
+let anoNascimento = prompt('Digite seu ano de nascimento');
+let anoAtual = 2023;
 
 console.log('Nome: ' + nome + '\n' + 'Idade: ' + idade + '\n' + 'Peso: ' + peso + '\n' + 'Altura: ' + altura + '\n' + 'Profissão: ' + prof);
 
@@ -21,16 +22,16 @@ console.log('Sua idade em dias: ' + (idade * 365));
 console.log('Seu imc é: ' + imc);
 
 switch(condicao){
-    case imc < 18,5:
+    case imc < '18,5':
         console.log('Magreza');
         break;
-    case imc >= 18,5 && imc <= 24,9:
+    case imc >= '18,5' && imc <= '24,9':
         console.log('Normal');
         break;
-    case imc > 24,9 && imc <= 30:
+    case imc > '24,9' && imc <= '30':
         console.log('Sobrepeso')
         break;
-    case imc > 30:
+    case imc > '30':
         console.log('Obesidade');
         break;
     default:
@@ -38,3 +39,9 @@ switch(condicao){
         break;
 }
 console.log('Você nasceu no ano: ' + (idade - 2023));
+
+let idadeAtual = 0;
+for (let ano = anoNascimento; ano <= anoAtual; ano++) {
+    console.log(`Ano: ${ano} - Idade: ${idadeAtual}`);
+    idadeAtual++;
+}
