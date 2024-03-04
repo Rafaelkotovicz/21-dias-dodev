0d7106c72958c41fe2d54d0ad21f4d6ac51c0242
Rafael Kotovicz <rafaelrck21@gmail.com>
2024-03-03 23:08:23 -0300

    adicionando arquivo ex2 do dia 09

--- dia09/ex/ex1.js
+++ dia09/ex/ex1.js
@@ -1,10 +1,9 @@
 let nome = prompt('Digite seu nome');
-let idade = prompt('Digite sua idade');
-let peso = prompt('Digite seu peso');
-let altura = prompt('Digite sua altura');
+let idade = Number(prompt('Digite sua idade'));
+let peso = Number(prompt('Digite seu peso'));
+let altura = Number(prompt('Digite sua altura'));
 let prof = prompt('Digite sua profissão');
 let imc = peso / (altura * altura);
-let condicao;
 let anoNascimento = prompt('Digite seu ano de nascimento');
 let anoAtual = 2023;
 
@@ -21,27 +20,71 @@ console.log('Sua idade em semanas: ' + (idade * 7));
 console.log('Sua idade em dias: ' + (idade * 365));
 console.log('Seu imc é: ' + imc);
 
-switch(condicao){
-    case imc < '18,5':
-        console.log('Magreza');
-        break;
-    case imc >= '18,5' && imc <= '24,9':
-        console.log('Normal');
-        break;
-    case imc > '24,9' && imc <= '30':
-        console.log('Sobrepeso')
-        break;
-    case imc > '30':
-        console.log('Obesidade');
-        break;
-    default:
-        console.log('Erro');
-        break;
+if(imc < 18,5){
+    console.log('Magreza');
+}else if(imc >= 18,5 && imc <= 24,9){
+    console.log('Normal');
+}else if(imc > 24,9 && imc <= 30){
+    console.log('Sobrepeso');
+}else if(imc > 30){
+    console.log('Obesidade');
+}else{
+    console.log('Erro');
 }
-console.log('Você nasceu no ano: ' + (idade - 2023));
+        
+console.log('Você nasceu no ano: ' + (2023 - idade ));
 
 let idadeAtual = 0;
 for (let ano = anoNascimento; ano <= anoAtual; ano++) {
     console.log(`Ano: ${ano} - Idade: ${idadeAtual}`);
     idadeAtual++;
 }
+
+let continuar = 's'
+
+ while(continuar == 's'){
+    
+    let nome = prompt('Digite seu nome');
+let idade = Number(prompt('Digite sua idade'));
+let peso = Number(prompt('Digite seu peso'));
+let altura = Number(prompt('Digite sua altura'));
+let prof = prompt('Digite sua profissão');
+let imc = peso / (altura * altura);
+let anoNascimento = prompt('Digite seu ano de nascimento');
+let anoAtual = 2023;
+
+console.log('Nome: ' + nome + '\n' + 'Idade: ' + idade + '\n' + 'Peso: ' + peso + '\n' + 'Altura: ' + altura + '\n' + 'Profissão: ' + prof);
+
+if(idade >= 18){
+    console.log('Pode beber');
+}else{
+    console.log('Não pode beber');
+}
+
+console.log('Sua idade em meses: ' + (idade * 12));
+console.log('Sua idade em semanas: ' + (idade * 7));
+console.log('Sua idade em dias: ' + (idade * 365));
+console.log('Seu imc é: ' + imc);
+
+if(imc < 18,5){
+    console.log('Magreza');
+}else if(imc >= 18,5 && imc <= 24,9){
+    console.log('Normal');
+}else if(imc > 24,9 && imc <= 30){
+    console.log('Sobrepeso');
+}else if(imc > 30){
+    console.log('Obesidade');
+}else{
+    console.log('Erro');
+}
+        
+console.log('Você nasceu no ano: ' + (2023 - idade ));
+
+let idadeAtual = 0;
+for (let ano = anoNascimento; ano <= anoAtual; ano++) {
+    console.log(`Ano: ${ano} - Idade: ${idadeAtual}`);
+    idadeAtual++;
+}
+
+    continuar = prompt('Deseja inserir os dados novamente?(s/n)');
+ }
