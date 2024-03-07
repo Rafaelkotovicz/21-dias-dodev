d9bd06cc03efbb1586542a268a277bd627de5dfb
Rafael Kotovicz <rafaelrck21@gmail.com>
2024-03-06 23:44:53 -0300

    refatorando arquivos so dia 09

--- dia09/ex/ex1.js
+++ dia09/ex/ex1.js
@@ -20,11 +20,11 @@ console.log('Sua idade em semanas: ' + (idade * 7));
 console.log('Sua idade em dias: ' + (idade * 365));
 console.log('Seu imc é: ' + imc);
 
-if(imc < 18,5){
+if(imc < 18.5){
     console.log('Magreza');
-}else if(imc >= 18,5 && imc <= 24,9){
+}else if(imc >= 18.5 && imc <= 24.9){
     console.log('Normal');
-}else if(imc > 24,9 && imc <= 30){
+}else if(imc > 24.9 && imc <= 30){
     console.log('Sobrepeso');
 }else if(imc > 30){
     console.log('Obesidade');
