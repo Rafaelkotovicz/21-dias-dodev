4e86a515d5db56d1e208e9380e8dfff3564c23b3
Rafael Kotovicz <rafaelrck21@gmail.com>
2024-03-10 17:32:22 -0300

    finalizando dia 11

--- dia11/ex/ex02.js
+++ dia11/ex/ex02.js
@@ -6,16 +6,19 @@ let contador = 0;
 
 while(continuar){
     let modelo = prompt('Digite o modelo do carro');
-    let ano = prompt('Digite o ano do carro')
-    let valor = prompt('Digite o valor do carro')
-
     modelos[contador] = modelo;
+    let ano = prompt('Digite o ano do carro');
     anos[contador] = ano;
+    let valor = prompt('Digite o valor do carro');
     valores[contador] = valor;
+    contador++;
+
     let condicao = prompt('Deseja cadastrar mais algum carro?')
     if(condicao != 's'){
         continuar = false;
     }
-    contador++;
 }
-console.log(modelos + '\n' + anos + '\n' + valores);
+console.log('Cadastro de carros:');
+for(let i = 0; i < modelos.length; i++){
+    console.log(modelos[i] + ' ' + anos[i] + ' ' + valores[i]);
+}
